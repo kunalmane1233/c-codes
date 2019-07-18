9c45e95a059ac832fa679d11855edd754d14e0db
kunalmane1233 <53033810+kunalmane1233@users.noreply.github.com>
2019-07-18 12:30:10 +0530

    ketan.c

---
+++ ketan.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("welcome to horizon 2k19");
+int a,b,c,d;
+printf("Enter the value of A :",a);
+scanf("%d",a);
+printf("Enter the value of B :",b);
+scanf("%d",b);
+c=a+b;
+d=a-b;
+printf("The Addition Of A and B is:%d",c);
+printf("The Subtraction Of A and B is :%d",d);
+getch();
+}
