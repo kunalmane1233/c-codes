95317a59120c0c0a135d0625c33bcfa6de0afffc
kunalmane1233 <53033810+kunalmane1233@users.noreply.github.com>
2019-07-18 12:39:44 +0530

    Update ketan.c

--- ketan.c
+++ ketan.c
@@ -5,9 +5,9 @@ void main()
 printf("welcome to horizon 2k19");
 int a,b,c,d,e,f;
 printf("Enter the value of A :",a);
-scanf("%d",a);
+scanf("%d",&a);
 printf("Enter the value of B :",b);
-scanf("%d",b);
+scanf("%d",&b);
 c=a+b;
 d=a-b;
 e=a/b;
